a21eeebef4b7fd0b0cce4dda600d9aa908fb438d
OGPoyraz <omergoktugpoyraz@gmail.com>
2016-09-08 14:47:31 +0300

    Initial commit 🐺

---
+++ fakeComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class FakeComponent extends React.Component {
+    render() {
+        return (
+            <div>I'm testing hold tight!</div>
+        )
+    }
+}
+export default FakeComponent;
